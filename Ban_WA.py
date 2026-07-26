5cfd279ef235b3d9218dca9428736596cc41137f
kisaragikiyzen-webdevelopment-id <kisaragikiyzen.id@gmail.com>
2026-07-26 19:51:53 +0700

    Update Ban_WA.py

--- Ban_WA.py
+++ Ban_WA.py
@@ -1,1 +1,85 @@
+# !!! Tools ini hanya untuk tujuan edukasi saja dan tak boleh disalah-gunakan !!!
+# !!! Tools ini gratis dan tak boleh dijual-belikan sama sekali !!!
+# !!! Tools ini tak boleh diubah kecuali Entitas Cyber karna bisa saja rusak !!!
+# !!! Perhatian, Jika Bot terkena limit, ganti Email dan App Password Bot dengan milik sendiri agar tetap bisa berjalan !!!
+# !!! App Password: https://myaccount.google.com/apppasswords? !!!
+# !!! Pastikan akun Email yang ingin dipakai harus terverifikasi dua langkah !!!
+# !!! © 2026 KiyzenBot • Created By 𝙆𝙞𝙨𝙖𝙧𝙖𝙜𝙞 𝙆𝙞𝙮𝙯𝙚𝙣 !!!
 
+import os
+import smtplib
+from colorama import Fore, Style, init
+from email.message import EmailMessage
+
+os.system("clear")
+init(autoreset=True)
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+# Email dan App Password Bot secara bawaan.
+SENDER = "gmail.bot.by.kisaragi.kiyzen@gmail.com"
+APP_PASSWORD = "ugws kpvi wayd xqxm"
+
+# Target spam disini.
+RECEIVER = "noreply@support.whatsapp.com"
+
+Banner = """   ██████╗  █████╗ ███╗   ██╗    ██╗    ██╗ █████╗
+   ██╔══██╗██╔══██╗████╗  ██║    ██║    ██║██╔══██╗
+   ██████╔╝███████║██╔██╗ ██║    ██║ █╗ ██║███████║
+   ██╔══██╗██╔══██║██║╚██╗██║    ██║███╗██║██╔══██║
+   ██████╔╝██║  ██║██║ ╚████║    ╚███╔███╔╝██║  ██║
+   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝     ╚══╝╚══╝ ╚═╝  ╚═╝
+    © 2026 KiyzenBot • Created By 𝙆𝙞𝙨𝙖𝙧𝙖𝙜𝙞 𝙆𝙞𝙮𝙯𝙚𝙣
+"""
+
+print(Fore.MAGENTA + Banner)
+
+reported_number = input("kiyzen@bot:~$ ").strip()
+
+# Judul dan Pesan yang dikirim.
+SUBJECT = "Laporan pengguna yang melanggar kebijakan WhatsApp!"
+BODY = """Halo Tim WhatsApp,
+
+Saya ingin melaporkan sebuah akun WhatsApp yang saya duga telah melakukan pelanggaran terhadap Ketentuan Layanan dan Kebijakan WhatsApp.
+
+Nomor yang dilaporkan: """ + reported_number + """\n\nAlasan Pelaporan:
+- Spam tanpa henti
+- Penipuan berulang-kali
+- Phishing dan Malware
+- penyamaran identitas
+- Aktivitas merugikan dan mencurigakan lainnya
+
+Saya memohon agar tim WhatsApp melakukan peninjauan terhadap akun tersebut. Apabila setelah dilakukan pemeriksaan ditemukan adanya pelanggaran terhadap kebijakan yang berlaku, saya berharap tindakan yang sesuai dapat diterapkan.
+
+Terima kasih.
+"""
+
+msg = EmailMessage()
+msg["From"] = SENDER
+msg["To"] = RECEIVER
+msg["Subject"] = SUBJECT
+msg.set_content(BODY)
+
+print("\n[+] System Connecting...")
+
+try:
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
+        smtp.starttls()
+        print("[-] System Connected ✓")
+
+        smtp.login(SENDER, APP_PASSWORD)
+        print("[-] Login Success ✓")
+
+        # Loading...
+        print("[+] Sending E-Mail...")
+        os.system("clear")
+        print(Fore.MAGENTA + Banner)
+        print("[+] Wait...")
+
+        while True:
+            smtp.send_message(msg)
+            print("[+] Spam Report...")
+
+except Exception as e:
+    print("[!] Error:", e)
